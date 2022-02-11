8dd520a75beeed6858208abebd948a715b8d4d4e
jromancalero <jromancalero@gmail.com>
2022-02-11 17:28:37 +0100

    primer

--- database/migrations/2022_02_09_152219_create_adressess_table.php
+++ database/migrations/2022_02_09_152219_create_adressess_table.php
@@ -13,7 +13,7 @@ class CreateAdressessTable extends Migration
      */
     public function up()
     {
-        Schema::create('_adressess', function (Blueprint $table) {
+        Schema::create('adressess', function (Blueprint $table) {
             $table->id();
             $table->string('adress');
             $table->timestamps();
@@ -27,6 +27,6 @@ class CreateAdressessTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('_adressess');
+        Schema::dropIfExists('adressess');
     }
 }
