663a493a495ee43e47f6f2ad6284829cf268726e
jromancalero <jromancalero@gmail.com>
2022-02-15 19:19:33 +0100

    jorge

--- resources/js/app.js
+++ resources/js/app.js
@@ -24,20 +24,5 @@ function topFunction() {
 document.body.scrollTop = 0;
 document.documentElement.scrollTop = 0;
 }
-    /* Función Slider */
-let slideIndex = 0;
-carousel();
-
-function carousel() {
-let i;
-let x = document.querySelector(".mySlides");
-for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
-}
-slideIndex++;
-if (slideIndex > x.length) {slideIndex = 1}
-x[slideIndex-1].style.display = "block";
-setTimeout(carousel, 5000);
-}
 
 /* --------------------------------------------------------------------------------------------- */
